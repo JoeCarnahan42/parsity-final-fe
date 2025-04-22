7044eef0cf89b8a4beefd4f1326c06968af19d07
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-04-22 07:56:37 -0500

    install hookform

--- app/components/AuthForm.js
+++ app/components/AuthForm.js
@@ -1,10 +1,21 @@
+"use client";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 export const AuthForm = () => {
+  const userSchema = Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().required(),
+  });
+
   return (
     <>
       <form>
         <div className="form-group">
           <label>Email</label>
           <input className="form-control" type="email" required />
+          <input className="form-control" type="password" required />
         </div>
       </form>
     </>
