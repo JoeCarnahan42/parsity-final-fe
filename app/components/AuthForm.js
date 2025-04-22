4703b41525d07e629611a3eb518cda80b4c1e6ca
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-04-22 07:40:33 -0500

    Install Yup

---
+++ app/components/AuthForm.js
@@ -0,0 +1,12 @@
+export const AuthForm = () => {
+  return (
+    <>
+      <form>
+        <div className="form-group">
+          <label>Email</label>
+          <input className="form-control" type="email" required />
+        </div>
+      </form>
+    </>
+  );
+};
